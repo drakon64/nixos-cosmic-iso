d6a3a6a273815c9644f97b60eab046f886702c76
Adam Chance <6444703+drakon64@users.noreply.github.com>
2024-07-12 13:45:40 +0100

    Attempt automatic daily builds

---
+++ flake.nix
@@ -0,0 +1,63 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    nixos-cosmic = {
+      url = "github:drakon64/nixos-cosmic";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, nixos-cosmic }: let
+    forAllSystems = nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" ];
+  in
+  {
+    legacyPackages = forAllSystems (system: let pkgs = nixpkgs.legacyPackages.${system}; in {
+      cosmicIso = let
+        nixosConfig = nixpkgs.lib.nixosSystem {
+          modules = [
+            ({ lib, pkgs, modulesPath, ... }: {
+              imports = [
+                nixos-cosmic.nixosModules.default
+
+                "${builtins.toString modulesPath}/installer/cd-dvd/installation-cd-graphical-base.nix"
+              ];
+
+              boot = {
+                kernelPackages = pkgs.linuxPackages_latest;
+
+                loader.grub.memtest86.enable = lib.mkForce false;
+
+                supportedFilesystems.zfs = lib.mkForce false;
+              };
+
+              hardware.pulseaudio.enable = lib.mkForce false;
+
+              isoImage.edition = lib.mkForce "cosmic";
+
+              nixpkgs.hostPlatform = system;
+
+              services = {
+                desktopManager.cosmic.enable = true;
+
+                displayManager = {
+                  autoLogin = {
+                    enable = true;
+
+                    user = "nixos";
+                  };
+
+                  cosmic-greeter.enable = true;
+                };
+
+                xserver.excludePackages = [ pkgs.xterm ];
+              };
+
+              system.stateVersion = lib.trivial.release;
+            })
+          ];
+        };
+      in nixosConfig.config.system.build.isoImage // { closure = nixosConfig.config.system.build.isoImage; inherit (nixosConfig) config pkgs; };
+    });
+  };
+}
