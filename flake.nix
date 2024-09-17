33a40402047a4f05eddba7cea3fe6e5d261f3ebe
Adam Chance <6444703+drakon64@users.noreply.github.com>
2024-09-17 15:23:53 +0100

    Apply formatting to Flake file

--- flake.nix
+++ flake.nix
@@ -8,52 +8,81 @@
     };
   };
 
-  outputs = { self, nixpkgs, nixos-cosmic }: let
-    forAllSystems = nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" ];
-  in
-  {
-    legacyPackages = forAllSystems (system: let pkgs = nixpkgs.legacyPackages.${system}; in {
-      cosmicIso = let
-        nixosConfig = nixpkgs.lib.nixosSystem {
-          modules = [
-            ({ lib, pkgs, modulesPath, ... }: {
-              imports = [
-                nixos-cosmic.nixosModules.default
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixos-cosmic,
+    }:
+    let
+      forAllSystems = nixpkgs.lib.genAttrs [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+    in
+    {
+      legacyPackages = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          cosmicIso =
+            let
+              nixosConfig = nixpkgs.lib.nixosSystem {
+                modules = [
+                  (
+                    {
+                      lib,
+                      pkgs,
+                      modulesPath,
+                      ...
+                    }:
+                    {
+                      imports = [
+                        nixos-cosmic.nixosModules.default
 
-                "${builtins.toString modulesPath}/installer/cd-dvd/installation-cd-graphical-base.nix"
-              ];
+                        "${builtins.toString modulesPath}/installer/cd-dvd/installation-cd-graphical-base.nix"
+                      ];
 
-              boot.loader.grub.memtest86.enable = lib.mkForce false;
+                      boot.loader.grub.memtest86.enable = lib.mkForce false;
 
-              hardware.pulseaudio.enable = lib.mkForce false;
+                      hardware.pulseaudio.enable = lib.mkForce false;
 
-              isoImage.edition = lib.mkForce "cosmic";
+                      isoImage.edition = lib.mkForce "cosmic";
 
-              nixpkgs.hostPlatform = system;
+                      nixpkgs.hostPlatform = system;
 
-              services = {
-                desktopManager.cosmic.enable = true;
+                      services = {
+                        desktopManager.cosmic.enable = true;
 
-                displayManager = {
-                  autoLogin = {
-                    enable = true;
+                        displayManager = {
+                          autoLogin = {
+                            enable = true;
 
-                    user = "nixos";
-                  };
+                            user = "nixos";
+                          };
 
-                  cosmic-greeter.enable = true;
-                };
+                          cosmic-greeter.enable = true;
+                        };
 
-                flatpak.enable = true;
+                        flatpak.enable = true;
 
-                xserver.excludePackages = [ pkgs.xterm ];
+                        xserver.excludePackages = [ pkgs.xterm ];
+                      };
+
+                      system.stateVersion = lib.trivial.release;
+                    }
+                  )
+                ];
               };
-
-              system.stateVersion = lib.trivial.release;
-            })
-          ];
-        };
-      in nixosConfig.config.system.build.isoImage // { closure = nixosConfig.config.system.build.isoImage; inherit (nixosConfig) config pkgs; };
-    });
-  };
+            in
+            nixosConfig.config.system.build.isoImage
+            // {
+              closure = nixosConfig.config.system.build.isoImage;
+              inherit (nixosConfig) config pkgs;
+            };
+        }
+      );
+    };
 }
